3128b2573e3e7566319442ac7e92ace1508b09ff
dnanba <31435597+dnanba@users.noreply.github.com>
2017-08-29 11:48:34 +0300

    Create palyndromes.py

---
+++ palyndromes.py
@@ -0,0 +1,19 @@
+word = input("Enter your word").lower() #getting the word and making it lowercase
+
+#checking length
+length = len(word)
+
+#creating intermediate list
+intermediate_word = list(word)
+
+
+for letter in word: #looping through word letters
+  length -= 1 #length counter -1
+  intermediate_word[length] = letter #substituting with current letter
+
+new_word = "".join(intermediate_word) #concatenating list to the new word
+
+print("%s backwards is %s" % (word, new_word))
+
+if new_word == word: #Palyndrome checking
+  print("It's a palyndrome!")
